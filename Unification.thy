6059be6e991177dfc0f002fb9375a7f4c3d98164
Omkar Z <omzade@student.ethz.com>
2023-04-20 12:56:27 +0200

    Add Assignment 1(a)

--- Unification.thy
+++ Unification.thy
@@ -1,5 +1,24 @@
 theory Unification
   imports Main
 begin
-value "1+(2::nat)"
+
+section \<open>Unification\<close>
+
+subsection \<open>Assignment 1\<close>
+
+datatype ('f,'v) "term" = Var 'v | Fun 'f "('f,'v) term list"
+
+(*just some random terms to get a feel for the datatype*)
+term "Var (1::nat)"
+term "Fun a [Var (1::nat), (Fun a [Var (1::nat)])]"
+
+value "\<Union> {{1::nat}, {2::nat}}"
+
+fun fv :: "('f,'v) term \<Rightarrow> 'v set" where
+"fv (Var x) = {x}"
+| "fv (Fun f ts) = (\<Union>t\<in>set(ts). fv t)"
+
+
+value "fv (Fun a [Var (1::nat), (Fun a [Var (2::nat)])])"
+
 end
