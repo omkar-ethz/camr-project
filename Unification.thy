e02979d6c625703f3422124f2f2c0eb8370a3570
Omkar Z <omzade@student.ethz.com>
2023-04-20 13:50:56 +0200

    Add assignment 1(b). Copy lemmas of 1(c) and oops them

--- Unification.thy
+++ Unification.thy
@@ -18,7 +18,40 @@ fun fv :: "('f,'v) term \<Rightarrow> 'v set" where
 "fv (Var x) = {x}"
 | "fv (Fun f ts) = (\<Union>t\<in>set(ts). fv t)"
 
-
 value "fv (Fun a [Var (1::nat), (Fun a [Var (2::nat)])])"
+
+type_synonym ('f,'v) subst = "'v \<Rightarrow> ('f,'v) term"
+
+fun sapply :: "('f,'v) subst \<Rightarrow> ('f,'v) term \<Rightarrow> ('f,'v) term" (infixr "\<cdot>" 67)
+  where
+"\<sigma> \<cdot> (Var x) = \<sigma> x"
+| "\<sigma> \<cdot> Fun f ts = Fun f (map (sapply \<sigma>) ts)"
+
+fun scomp :: "('f,'v) subst \<Rightarrow> ('f,'v) subst \<Rightarrow> ('f,'v) subst" (infixl "\<circ>\<^sub>s" 75)
+  where 
+"(\<sigma> \<circ>\<^sub>s \<tau>) x = \<sigma> \<cdot> \<tau> x"
+
+lemma fv_sapply: "fv (\<sigma> \<cdot> t) = (\<Union>x \<in> fv t. fv (\<sigma> x))"
+  oops
+
+lemma sapply_cong:
+  assumes a: "\<And>x. x \<in> fv t \<Longrightarrow> \<sigma> x = \<tau> x"
+  shows "\<sigma> \<cdot> t = \<tau> \<cdot> t"
+  oops
+
+lemma scomp_sapply: "(\<sigma> \<circ>\<^sub>s \<tau> ) x = \<sigma> \<cdot> (\<tau> x)"
+  by simp
+
+lemma sapply_scomp_distrib: "(\<sigma> \<circ>\<^sub>s \<tau> ) \<cdot> t = \<sigma> \<cdot> (\<tau> \<cdot> t)"
+  by (induction t) simp_all
+
+lemma scomp_assoc: "(\<sigma> \<circ>\<^sub>s \<tau>) \<circ>\<^sub>s \<rho> =  \<sigma> \<circ>\<^sub>s (\<tau> \<circ>\<^sub>s \<rho>)"
+  oops
+
+lemma scomp_Var [simp]: "\<sigma> \<circ>\<^sub>s Var = \<sigma>"
+  oops
+
+lemma Var_scomp [simp]: "Var \<circ>\<^sub>s \<sigma>= \<sigma>"
+  oops
 
 end
